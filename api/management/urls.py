10b343b6e4314cc75a6dcbaad3b36898d0c26567
Stephen Nene <stephen.nene@student.moringaschool.com>
2025-03-29 22:12:30 +0300

    refactor: remove logs app and update database name to tiberbu.db

---
+++ api/management/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from management.views import TripViewSet, LogSheetViewSet
+
+router = routers.DefaultRouter()
+router.register(r'trips', TripViewSet)
+router.register(r'logs', LogSheetViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('api/', include(router.urls)),
+]
