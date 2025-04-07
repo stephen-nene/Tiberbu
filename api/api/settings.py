6989c90b3bc523cd324d2c541d7f45acd5ff9e1e
stephen-nene <stephen.nene@student.moringaschool.com>
2025-04-08 01:10:01 +0300

    Refactor STATIC_ROOT definition to use os.path.join for better compatibility

--- api/api/settings.py
+++ api/api/settings.py
@@ -243,7 +243,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
+# STATIC_ROOT = BASE_DIR/ 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
